9127cea57af92332bb51f3b9eab7d59822621a06
James Sanders <james.sanders@thisismedium.com>
2010-09-01 16:55:15 -0400

    Removed restler dependency

--- test/test.js
+++ test/test.js
@@ -1,8 +1,7 @@
-var rio = require("../src/relay.io");
-var it  = require("../src/lib/iterators");
+var rio = require("node-relay-client");
+var it  = require("../deps/iterators.js/src/iterators");
 
 var relay = new rio.RelayIO("73B44EF3-CED8-400D-AB73-ED9397E4B718");
-
 it.eachAsync(it.range(0,50), function (i, next){
     relay.insert("1C471305-B172-4DDC-993D-893E3F4D5010", '{"test":"'+i+'"}', next);
 })
